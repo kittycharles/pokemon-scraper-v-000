e8bec03191d54ba0c984ed07eeafa2897ba488be
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-12 16:15:38 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,12 +1,12 @@
 class Pokemon
-attr_accessor :id, :name, :type, :db 
+attr_accessor :id, :name, :type, :db
 
-  def initialize(id, name, type, db)
-    @id = id
-    @name = name
-    @type = type
-    @ds = db
-  end
+def initialize(id, name, type, db)
+  @id = id
+  @name = name
+  @type = type
+  @db = db
+end
 
   def save
   end
